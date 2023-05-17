415e1860460c6b0817f70df7721f4f6aa060a909
Jason Lee <jasonepic0801@gmail.com>
2023-05-17 16:39:31 +0800

    Setup router and login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,15 @@ import './App.css';
 // this is how we link to the backend django server
 // we can use this to make requests to the backend, using axios.get() or axios.post()
 import axios from "axios"; 
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Outlet>
+          
+        </Outlet>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
